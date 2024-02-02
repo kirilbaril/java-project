721973d5c8049ef36d4091a87ad0704153cfbf0c
kirilbaril <146415394+kirilbaril@users.noreply.github.com>
2024-02-02 09:44:50 +0200

    Add files via upload

---
+++ 02.02.24/Paralelogram.java
@@ -0,0 +1,13 @@
+public class Paralelogram extends Shape {
+    private double height;
+    private double weight;
+    public Paralelogram(double height, double weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public double getArea() {
+        return height * weight;
+    }
+}
